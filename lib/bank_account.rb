9a04043c358e3156f76012c35f771252651581b2
Marley Zelinger <marley.zelinger@gmail.com>
2017-06-09 19:13:07 +0000

    banking kwk finished lab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
+require_relative 'transfer'
 class BankAccount
-  # code here
+    attr_reader :name, :status
+    attr_accessor :balance, :status
+    def initialize(name)
+      @name = name
+      @balance = 1000
+      @status = "open"
+    end
+    def deposit(amount)
+        @balance = amount + @balance
+    end
+    def display_balance
+        "Your Balance is $#{balance}."
+    end 
+    def valid?
+        if @balance <= 0
+            false
+        elsif @status == "closed"
+            false
+        else
+            true
+        end
+    end
+    def close_account
+        @status = "closed"
+    end
 end
+
+acc1 = BankAccount.new("marley")
+acc2 = BankAccount.new("other")
+t1 = Transfer.new(acc1, acc2, 10000)
+
+puts t1.execute_transaction
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
 class Transfer
-  # code here
+    attr_reader :sender, :receiver, :amount, :done
+    attr_accessor :status
+    def initialize(sender, receiver, amount)
+      @sender = sender
+      @receiver = receiver
+      @amount = amount
+      @status = "pending"
+      @done = false
+    end
+    def both_valid?
+        if sender.valid? && receiver.valid?
+            true
+        else
+            false
+        end
+    end
+    def execute_transaction
+        if @done == false
+            @done = true
+            if both_valid? && @sender.balance >= amount
+                @status = "complete"
+                @receiver.balance = receiver.balance + amount
+                @sender.balance = sender.balance - amount
+            else
+                puts "hit"
+                @status = "rejected"
+                "Transaction rejected. Please check your account balance."
+            end
+        end
+    end
+    def reverse_transfer
+        if @done == true
+        @status = "reversed"
+        @receiver.balance = receiver.balance - amount
+        @sender.balance = sender.balance + amount
+        end
+    end
+    
 end
+
